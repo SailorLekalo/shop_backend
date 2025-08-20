ca6141c89289246f2e5b96e18719622d333d4905
SailorLekalo <shamshev-andrei@yandex.ru>
2025-08-20 21:14:59 +0300

    Added config and .env

---
+++ app/settings/config.py
@@ -0,0 +1,26 @@
+from dotenv import load_dotenv
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class BaseConfig(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        extra="ignore"
+    )
+
+
+class DBConfig(BaseConfig):
+    load_dotenv()
+    DB_USER: str
+    DB_PASSWORD: str
+    DB_NAME: str
+    DB_HOST: str = "db"
+    SESSION_EXPIRE_MINUTES: int = 1440
+
+    @property
+    def database_url(self) -> str:
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:5432/{self.DB_NAME}"
+
+
+config = DBConfig()
