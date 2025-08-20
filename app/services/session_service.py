5fa8a030181ade7f2f01f8efde9a23e872958f65
SailorLekalo <shamshev-andrei@yandex.ru>
2025-08-20 21:30:44 +0300

    Убрана избыточная логика в session_service

--- app/services/session_service.py
+++ app/services/session_service.py
@@ -15,31 +15,27 @@ class SessionError:
 
 
 class SessionService:
-    async def check_session_expiration(self, db: AsyncSessionLocal, check_ses: Session):
-        async with db as session:
-            check = await session.execute(select(Session).where(Session.ses_id == check_ses.ses_id))
-            check = check.scalars().first()
-            if check.expires_at > datetime.now():
-                return False
-            return True
+    async def check_session_expiration(self, check_ses: Session):
+        if check_ses.expires_at > datetime.now():
+            return False
+        return True
 
     async def user_by_session(self, info: Info, db: AsyncSessionLocal) -> User | SessionError:
-        async with db as session:
-            request = info.context["request"]
-            auth_header = request.headers.get("authorization")
 
-            sessionid = None
-            if auth_header and auth_header.startswith("Bearer "):
-                sessionid = auth_header[len("Bearer "):]
+        request = info.context["request"]
+        auth_header = request.headers.get("authorization")
+        sessionid = None
 
-            check = await session.execute(select(Session).where(Session.ses_id == sessionid))
-            check = check.scalars().first()
+        if auth_header and auth_header.startswith("Bearer "):
+            sessionid = auth_header[len("Bearer "):]
+        check = await db.execute(select(Session).where(Session.ses_id == sessionid))
+        check = check.scalars().first()
 
-            if check is None:
-                return SessionError(message="Сессия не существует")
+        if check is None:
+            return SessionError(message="Сессия не существует")
 
-            if await self.check_session_expiration(db, check):
-                return SessionError(message="Сессия истекла")
+        if await self.check_session_expiration(check):
+            return SessionError(message="Сессия истекла")
+        user = await db.execute(select(User).where(User.id == check.user_id))
 
-            user = await session.execute(select(User).where(User.id == check.user_id))
-            return user.scalars().first()
+        return user.scalars().first()
